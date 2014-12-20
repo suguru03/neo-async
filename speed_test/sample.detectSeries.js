fb9080266705c616b05eb48fc37705c5cfda93c1
Suguru Motegi <hollow.03.03.bleach@gmail.com>
2014-12-20 22:43:57 +0900

    Modify response of detect, detectSeries and detectLimit

---
+++ speed_test/sample.detectSeries.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node --stack-size=65536
+'use strict';
+var comparator = require('func-comparator');
+var _ = require('lodash');
+var async = require('async');
+var neo_async = require('../');
+
+// roop count
+var count = 10;
+// sampling times
+var times = 300;
+var array = _.sample(_.times(count), count);
+var iterator = function(n, callback) {
+  callback(null, false);
+};
+var funcs = {
+  'async': function(callback) {
+    async.detectSeries(array, iterator, callback);
+  },
+  'neo-async': function(callback) {
+    neo_async.detectSeries(array, iterator, callback);
+  }
+};
+
+comparator
+.set(funcs)
+.option({
+  async: true,
+  times: times
+})
+.start()
+.result(function(err, res) {
+  console.log(res);
+});
+
+
