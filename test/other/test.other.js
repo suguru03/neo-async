f7c78207984308fd8fa03a997b48f8a9208873a3
Suguru Motegi <hollow.03.03.bleach@gmail.com>
2015-01-08 10:43:43 +0900

    Add nextTick test

--- test/other/test.other.js
+++ test/other/test.other.js
@@ -18,12 +18,12 @@ describe('#define', function() {
       assert.deepEqual(array, []);
       var _async = callback();
       assert.deepEqual(_.keys(async), _.keys(_async));
+      done();
     };
     define.amd = true;
     delete(require.cache[path]);
     require(path);
-    done();
-
+    define = null;
   });
 
 });
@@ -31,6 +31,29 @@ describe('#define', function() {
 describe('#nextTick', function() {
 
   'use strict';
+
+  it('should check nextTick if process does not have nextTick', function(done) {
+
+    var _nextTick = process.nextTick;
+    process.nextTick = undefined;
+    delete(require.cache[path]);
+    require(path);
+    process.nextTick = _nextTick;
+    done();
+  });
+
+  it('should check nextTick if process does not have nextTick and setImmediate', function(done) {
+
+    var _nextTick = process.nextTick;
+    var _setImmediate = setImmediate;
+    process.nextTick = undefined;
+    setImmediate = undefined;
+    delete(require.cache[path]);
+    require(path);
+    process.nextTick = _nextTick;
+    setImmediate = _setImmediate;
+    done();
+  });
 
   it('should check nextTick if does not have setImmediate', function(done) {
 
@@ -41,4 +64,5 @@ describe('#nextTick', function() {
     setImmediate = _setImmediate;
     done();
   });
+
 });
