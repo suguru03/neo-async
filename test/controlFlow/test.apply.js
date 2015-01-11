6197534af3b7275b5e0b4fc879f5721afbb40652
Suguru Motegi <hollow.03.03.bleach@gmail.com>
2015-01-11 17:42:56 +0900

    Fix versioning and add apply test

--- test/controlFlow/test.apply.js
+++ test/controlFlow/test.apply.js
@@ -25,5 +25,27 @@ describe('#apply', function() {
 
   });
 
+  it('should apply other object', function(done) {
+
+    var func = function(num1, num2, callback) {
+      assert.strictEqual(this, Math);
+      setTimeout(function(math) {
+        callback(null, math.round(num1 + num2));
+      }, num1 * 10, this);
+    };
+    async.parallel([
+      async.apply(func, 1).bind(Math, 1.1),
+      async.apply(func, 2).bind(Math, 1.3),
+      async.apply(func, 3).bind(Math, 1.5),
+      async.apply(func, 4).bind(Math, 1.7)
+    ], function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.deepEqual(res, [2, 3, 5, 6]);
+      done();
+    });
+  });
+
 });
 
