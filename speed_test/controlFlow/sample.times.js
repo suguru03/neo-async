2aed1667a003de715e2fd3bd5dcef2781f928366
Suguru Motegi <hollow.03.03.bleach@gmail.com>
2015-02-01 15:12:56 +0900

    Modify times to respond quickly

---
+++ speed_test/controlFlow/sample.times.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node --stack-size=65536
+'use strict';
+var comparator = require('func-comparator');
+var async = require('async');
+var neo_async = require('../../');
+
+// loop count
+var count = 100;
+// sampling times
+var times = 1000;
+var iterator = function(n, done) {
+  done(null, n);
+};
+
+var funcs = {
+  'async': function(callback) {
+    async.times(count, iterator, callback);
+  },
+  'neo-async': function(callback) {
+    neo_async.times(count, iterator, callback);
+  }
+};
+
+comparator
+.set(funcs)
+.option({
+  async: true,
+  times: times
+})
+.start()
+.result(function(err, res) {
+  console.log(res);
+});
+
+
