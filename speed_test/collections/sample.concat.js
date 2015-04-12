ac15f145e7af0ec70812628ddc0b4bdfd3fc472a
Suguru Motegi <suguru.motegi@gmail.com>
2015-04-12 17:20:15 +0900

    fix(concat): fix `concat` not to use common function

--- speed_test/collections/sample.concat.js
+++ speed_test/collections/sample.concat.js
@@ -4,34 +4,33 @@
 var comparator = require('func-comparator');
 var _ = require('lodash');
 var async = require('async');
-var neo_async = require('../../');
+var neo_async_v0 = require('neo-async');
+var neo_async_v1 = require('../../');
 
 // loop count
 var count = 100;
 // sampling times
-var times = 3000;
+var times = 100000;
 var array = _.shuffle(_.times(count));
-var c = 0;
 var iterator = function(n, callback) {
   callback(null, [n]);
 };
 var funcs = {
   'async': function(callback) {
-    c = 0;
     async.concat(array, iterator, callback);
   },
-  'neo-async': function(callback) {
-    c = 0;
-    neo_async.concat(array, iterator, callback);
+  'neo-async_v0': function(callback) {
+    neo_async_v0.concat(array, iterator, callback);
+  },
+  'neo-async_v1': function(callback) {
+    neo_async_v1.concat(array, iterator, callback);
   }
 };
 
 comparator
   .set(funcs)
-  .option({
-    async: true,
-    times: times
-  })
+  .async()
+  .times(times)
   .start()
   .result(function(err, res) {
     console.log(res);
