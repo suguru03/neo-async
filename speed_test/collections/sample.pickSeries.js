7020370b9c20176a4cfe7aaa88619b1caffb72b1
Suguru Motegi <suguru.motegi@gmail.com>
2015-04-13 00:44:32 +0900

    refactor(pickSeries): refactor `pickSeries` to improve response

--- speed_test/collections/sample.pickSeries.js
+++ speed_test/collections/sample.pickSeries.js
@@ -3,7 +3,6 @@
 'use strict';
 var comparator = require('func-comparator');
 var _ = require('lodash');
-var async = require('async');
 var neo_async_v0 = require('neo-async');
 var neo_async_v1 = require('../../');
 
@@ -12,26 +11,17 @@ var count = 100;
 // sampling times
 var times = 100000;
 var array = _.shuffle(_.times(count));
-var c = 0;
 var iterator = function(n, callback) {
-  callback(c++ % 2);
+  callback(false);
 };
 var funcs = {
-  'async': function(callback) {
-    c = 0;
-    async.filterSeries(array, iterator, function(res) {
-      callback(null, res);
-    });
-  },
   'neo-async_v0': function(callback) {
-    c = 0;
-    neo_async_v0.filterSeries(array, iterator, function(res) {
+    neo_async_v0.pickSeries(array, iterator, function(res) {
       callback(null, res);
     });
   },
   'neo-async_v1': function(callback) {
-    c = 0;
-    neo_async_v1.filterSeries(array, iterator, function(res) {
+    neo_async_v1.pickSeries(array, iterator, function(res) {
       callback(null, res);
     });
   }
@@ -39,10 +29,8 @@ var funcs = {
 
 comparator
   .set(funcs)
-  .option({
-    async: true,
-    times: times
-  })
+  .async()
+  .times(times)
   .start()
   .result(function(err, res) {
     console.log(res);
