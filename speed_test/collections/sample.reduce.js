835742c0836eb9fd1d933c8c1f3640be04e71f31
Suguru Motegi <suguru.motegi@gmail.com>
2015-04-13 19:16:34 +0900

    refactor(reduceRight): refactor `reduceRight` to improve response

--- speed_test/collections/sample.reduce.js
+++ speed_test/collections/sample.reduce.js
@@ -4,12 +4,13 @@
 var comparator = require('func-comparator');
 var _ = require('lodash');
 var async = require('async');
-var neo_async = require('../../');
+var neo_async_v0 = require('neo-async');
+var neo_async_v1 = require('../../');
 
 // loop count
-var count = 10;
+var count = 100;
 // sampling times
-var times = 1000;
+var times = 100000;
 var array = _.shuffle(_.times(count));
 var iterator = function(total, n, callback) {
   callback(null, total + n);
@@ -18,17 +19,18 @@ var funcs = {
   'async': function(callback) {
     async.reduce(array, 0, iterator, callback);
   },
-  'neo-async': function(callback) {
-    neo_async.reduce(array, 0, iterator, callback);
+  'neo-async_v0': function(callback) {
+    neo_async_v0.reduce(array, 0, iterator, callback);
+  },
+  'neo-async_v1': function(callback) {
+    neo_async_v1.reduce(array, 0, iterator, callback);
   }
 };
 
 comparator
   .set(funcs)
-  .option({
-    async: true,
-    times: times
-  })
+  .async()
+  .times(times)
   .start()
   .result(function(err, res) {
     console.log(res);
