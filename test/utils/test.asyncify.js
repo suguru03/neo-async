28ee06aa6065a3dc82564c873a0c03294a2556ba
Suguru Motegi <suguru.motegi@gmail.com>
2015-07-17 23:16:09 +0900

    feat(asyncify): fix to apply promise

--- test/utils/test.asyncify.js
+++ test/utils/test.asyncify.js
@@ -3,6 +3,7 @@
 
 var assert = require('power-assert');
 var async = require('../../');
+var Q = require('q');
 
 describe('#asyncify', function() {
 
@@ -13,6 +14,25 @@ describe('#asyncify', function() {
     };
     var func = function() {
       return test;
+    };
+    async.asyncify(func)(function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(res, test);
+      done();
+    });
+  });
+
+  it('should convert promise synchronous function', function(done) {
+
+    var test = {
+      hoge: true
+    };
+    var func = function() {
+      var d = Q.defer();
+      d.resolve(test);
+      return d.promise;
     };
     async.asyncify(func)(function(err, res) {
       if (err) {
@@ -34,6 +54,20 @@ describe('#asyncify', function() {
       assert.strictEqual(err.message, message);
       done();
     });
+  });
 
+  it('should throw error by promise', function(done) {
+
+    var message = 'error';
+    var func = function() {
+      var d = Q.defer();
+      d.reject(new Error(message));
+      return d.promise;
+    };
+    async.asyncify(func)(function(err) {
+      assert.ok(err);
+      assert.strictEqual(err.message, message);
+      done();
+    });
   });
 });
