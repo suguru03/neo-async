b6c6376dc60b81e407297596b19d40d6607d45b1
Suguru Motegi <suguru.motegi@gmail.com>
2016-03-20 17:18:56 -0700

    fix(autoInject): fix not to inject if task is array

--- test/controlFlow/test.autoInject.js
+++ test/controlFlow/test.autoInject.js
@@ -122,10 +122,11 @@ parallel('#autoInject', function() {
         assert.strictEqual(task3, 3);
         callback(null, 2);
       }],
-      task2_1: ['task3', function(callback) {
+      task2_1: ['task3', function(arg1, callback) {
         order.push('task2_1');
+        assert.strictEqual(arg1, 3);
         assert.ok(_.isFunction(callback));
-        assert.strictEqual(arguments.length, 1);
+        assert.strictEqual(arguments.length, 2);
         callback();
       }],
       task2_2: function(task3, callback) {
@@ -133,15 +134,15 @@ parallel('#autoInject', function() {
         assert.strictEqual(task3, 3);
         callback();
       },
-      task2_3: ['task1', 'task3', function(task3, task1, callback) {
+      task2_3: ['task1', 'task3', function(arg1, arg2, callback) {
         order.push('task2_3');
-        assert.strictEqual(task1, 1);
-        assert.strictEqual(task3, 3);
+        assert.strictEqual(arg1, 1);
+        assert.strictEqual(arg2, 3);
         callback();
       }],
-      task2_4: ['task3', function(task1, callback) {
+      task2_4: ['task3', function(arg1, callback) {
         order.push('task2_4');
-        assert.strictEqual(task1, 1);
+        assert.strictEqual(arg1, 3);
         callback();
       }],
       task3: function (callback) {
@@ -169,8 +170,10 @@ parallel('#autoInject', function() {
 
     var order = [];
     async.autoInject({
-      task1: ['task3', 'task2', function(callback) {
+      task1: ['task3', 'task2', function(arg1, arg2, callback) {
         order.push('task1');
+        assert.strictEqual(arg1, 3);
+        assert.strictEqual(arg2, 2);
         callback(null, 1);
       }],
       task2: function(task3, callback) {
@@ -178,8 +181,8 @@ parallel('#autoInject', function() {
         order.push('task2');
         callback(null, 2);
       },
-      task3: ['task5', function(task6, callback) {
-        assert.strictEqual(task6, 6);
+      task3: ['task5', function(arg1, callback) {
+        assert.strictEqual(arg1, 5);
         order.push('task3');
         callback(null, 3);
       }],
