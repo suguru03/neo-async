2716747f0dfc1463f23c800c9deacbbb16248df1
Suguru Motegi <suguru.motegi@gmail.com>
2016-04-14 19:56:20 -0700

    feat(applyEach): fix to return results like `async.map`

--- test/controlFlow/test.applyEach.js
+++ test/controlFlow/test.applyEach.js
@@ -11,36 +11,34 @@ parallel('#applyEach', function() {
   it('should execute in parallel', function(done) {
 
     var order = [];
-
     var one = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(1);
-        cb();
+        cb(null, 1);
       }, 100);
     };
-
     var two = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(2);
-        cb();
+        cb(null, 2);
       }, 50);
     };
-
     var three = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(3);
-        cb();
+        cb(null, 3);
       }, 150);
     };
 
-    async.applyEach([one, two, three], 5, function(err) {
+    async.applyEach([one, two, three], 5, function(err, res) {
       if (err) {
         return done(err);
       }
       assert.deepEqual(order, [2, 1, 3]);
+      assert.deepEqual(res, [1, 2, 3]);
       done();
     });
 
@@ -49,39 +47,36 @@ parallel('#applyEach', function() {
   it('should execute as a partial apprication', function(done) {
 
     var order = [];
-
     var one = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(1);
-        cb();
+        cb(null, 1);
       }, 100);
     };
-
     var two = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(2);
-        cb();
+        cb(null, 2);
       }, 50);
     };
-
     var three = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(3);
-        cb();
+        cb(null, 3);
       }, 150);
     };
 
-    async.applyEach([one, two, three])(5, function(err) {
+    async.applyEach([one, two, three])(5, function(err, res) {
       if (err) {
         return done(err);
       }
       assert.deepEqual(order, [2, 1, 3]);
+      assert.deepEqual(res, [1, 2, 3]);
       done();
     });
-
   });
 
 });
@@ -91,39 +86,36 @@ parallel('#applyEachSeries', function() {
   it('should execute in series', function(done) {
 
     var order = [];
-
     var one = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(1);
-        cb();
+        cb(null, 1);
       }, 100);
     };
-
     var two = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(2);
-        cb();
+        cb(null, 2);
       }, 50);
     };
-
     var three = function(val, cb) {
       assert.equal(val, 5);
       setTimeout(function() {
         order.push(3);
-        cb();
+        cb(null, 3);
       }, 150);
     };
 
-    async.applyEachSeries([one, two, three], 5, function(err) {
+    async.applyEachSeries([one, two, three], 5, function(err, res) {
       if (err) {
         return done(err);
       }
       assert.deepEqual(order, [1, 2, 3]);
+      assert.deepEqual(res, [1, 2, 3]);
       done();
     });
-
   });
 
 });
