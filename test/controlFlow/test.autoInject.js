b48e546f06859b4291883a9dcaf7b8171f999ed4
Suguru Motegi <suguru.motegi@gmail.com>
2016-05-26 23:28:18 -0700

    fix(autoInject): fix to support es6

--- test/controlFlow/test.autoInject.js
+++ test/controlFlow/test.autoInject.js
@@ -277,4 +277,36 @@ parallel('#autoInject', function() {
     assert.ok(err);
   });
 
+  var arrowSupport = true;
+  try {
+    /* jshint -W054 */
+    new Function('x => x');
+    /* jshint +W054 */
+  } catch (e) {
+    arrowSupport = false;
+  }
+
+  if (arrowSupport) {
+    // Needs to be run on ES6 only
+
+    /* jshint -W061 */
+    eval("(function() {                                             " +
+       "  it('should work with es6 arrow syntax', function (done) { " +
+       "    async.autoInject({                                      " +
+       "      task1: (cb)           => cb(null, 1),                 " +
+       "      task2: ( task3 , cb ) => cb(null, 2),                 " +
+       "      task3: cb             => cb(null, 3)                  " +
+       "    }, (err, results) => {                                  " +
+       "      assert.deepEqual(results, {                           " +
+       "        task1: 1,                                           " +
+       "        task2: 2,                                           " +
+       "        task3: 3                                            " +
+       "      });                                                   " +
+       "      done();                                               " +
+       "    });                                                     " +
+       "  });                                                       " +
+       "})                                                          "
+    )();
+    /* jshint +W061 */
+  }
 });
