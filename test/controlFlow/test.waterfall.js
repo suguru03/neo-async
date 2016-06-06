4ad5741d1213dd43504362943163d7324fc2e0b2
Suguru Motegi <suguru.motegi@gmail.com>
2016-06-05 23:27:53 -0700

    fix(waterfall): fix to avoid double callback #19

--- test/controlFlow/test.waterfall.js
+++ test/controlFlow/test.waterfall.js
@@ -201,6 +201,32 @@ parallel('#waterfall', function() {
     }, Math);
   });
 
+  it('should avoid double callback', function(done) {
+
+    var called = false;
+    var tasks = _.times(3, function(n) {
+      return function(callback) {
+        try {
+          callback('error' + n);
+        } catch (exception) {
+          try {
+            callback(exception);
+          } catch(e) {
+            assert.ok(e);
+            util.errorChecker(e);
+          }
+          done();
+        }
+      };
+    });
+    async.waterfall(tasks, function(err) {
+      assert.ok(err);
+      assert.strictEqual(called, false);
+      called = true;
+      async.nothing();
+    });
+  });
+
   it('should return response immediately if array task is empty', function(done) {
 
     var tasks = [];
