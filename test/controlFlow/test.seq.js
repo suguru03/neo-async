fbebcb799425b062e32ca53e59817849cf57a1d7
Suguru Motegi <suguru.motegi@gmail.com>
2016-06-07 23:33:13 -0700

    feat(seq): fix to execute without callback #20

--- test/controlFlow/test.seq.js
+++ test/controlFlow/test.seq.js
@@ -6,6 +6,7 @@ var assert = require('assert');
 var parallel = require('mocha.parallel');
 
 var async = global.async || require('../../');
+var delay = require('../config').delay;
 
 parallel('#seq', function() {
 
@@ -17,21 +18,18 @@ parallel('#seq', function() {
         cb(null, n + 2);
       }, 50);
     };
-
     var mul3 = function(n, cb) {
       assert.strictEqual(n, 5);
       setTimeout(function() {
         cb(null, n * 3);
       }, 15);
     };
-
     var add1 = function(n, cb) {
       assert.strictEqual(n, 15);
       setTimeout(function() {
         cb(null, n + 1);
       }, 100);
     };
-
     var add2mul3add1 = async.seq(add2, mul3, add1);
 
     add2mul3add1(3, function(err, res) {
@@ -41,7 +39,6 @@ parallel('#seq', function() {
       assert.strictEqual(res, 16);
       done();
     });
-
   });
 
   it('should execute in order of insertion with binding', function(done) {
@@ -53,14 +50,12 @@ parallel('#seq', function() {
         cb(null, self.pow(n, 2));
       }, 50);
     };
-
     var mul3 = function(n, cb) {
       assert.strictEqual(n, 9);
       setTimeout(function() {
         cb(null, n * 3);
       }, 15);
     };
-
     var pow3 = function(n, cb) {
       var self = this;
       assert.strictEqual(n, 27);
@@ -78,7 +73,25 @@ parallel('#seq', function() {
       assert.strictEqual(res, 19683);
       done();
     });
+  });
 
+  it('should execute without callback', function(done) {
+
+    var testcontext = {
+      name: 'foo'
+    };
+    var add2 = function (n, cb) {
+      assert.strictEqual(this, testcontext);
+      setTimeout(function () {
+        cb(null, n + 2);
+      }, delay * 2);
+    };
+    var mul3 = function () {
+      assert.strictEqual(this, testcontext);
+      setTimeout(done, delay);
+    };
+    var add2mul3 = async.seq(add2, mul3);
+    add2mul3.call(testcontext, 3);
   });
 
   it('should throw error', function(done) {
@@ -89,27 +102,23 @@ parallel('#seq', function() {
         cb(null, n + 2);
       }, 50);
     };
-
     var mul3 = function(n, cb) {
       assert.strictEqual(n, 5);
       setTimeout(function() {
         cb(new Error('error'));
       }, 15);
     };
-
     var add1 = function(n, cb) {
       setTimeout(function() {
         cb(null, n + 1);
       }, 100);
     };
-
     var add2mul3add1 = async.seq(add2, mul3, add1);
 
     add2mul3add1(3, function(err) {
       assert.ok(err);
       done();
     });
-
   });
 
 });
