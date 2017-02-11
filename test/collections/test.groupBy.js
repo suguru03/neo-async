1b6db005bb32cef81db6123e112ead6a6919bb22
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-10 20:03:17 -0800

    feat(groupBySeries): add groupBySeries

--- test/collections/test.groupBy.js
+++ test/collections/test.groupBy.js
@@ -368,3 +368,334 @@ parallel('#groupBy', function() {
   });
 
 });
+
+parallel('#groupBySeries', function() {
+
+  it('should execute iterator by collection of array', function(done) {
+
+    var order = [];
+    var collection = [4.2, 6.4, 6.1];
+    async.groupBySeries(collection, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [4.2],
+        '6': [6.4, 6.1]
+      });
+      assert.deepEqual(order, [4.2, 6.4, 6.1]);
+      done();
+    });
+  });
+
+  it('should execute iterator by collection of array with passing index', function(done) {
+
+    var order = [];
+    var collection = [4.2, 6.4, 6.1];
+    async.groupBySeries(collection, groupByIteratorWithKey(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [4.2],
+        '6': [6.4, 6.1]
+      });
+      assert.deepEqual(order, [
+        [4.2, 0],
+        [6.4, 1],
+        [6.1, 2]
+      ]);
+      done();
+    });
+  });
+
+  it('should execute iterator by collection of object', function(done) {
+
+    var order = [];
+    var collection = {
+      a: 4.2,
+      b: 6.4,
+      c: 6.1
+    };
+    async.groupBySeries(collection, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [4.2],
+        '6': [6.4, 6.1]
+      });
+      assert.deepEqual(order, [4.2, 6.4, 6.1]);
+      done();
+    });
+  });
+
+  it('should execute iterator by collection of object with key', function(done) {
+
+    var order = [];
+    var collection = {
+      a: 4.2,
+      b: 6.4,
+      c: 6.1
+    };
+    async.groupBySeries(collection, groupByIteratorWithKey(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [4.2],
+        '6': [6.4, 6.1]
+      });
+      assert.deepEqual(order, [
+        [4.2, 'a'],
+        [6.4, 'b'],
+        [6.1, 'c']
+      ]);
+      done();
+    });
+  });
+
+  it('should execute iterator to series by collection of Set', function(done) {
+
+    var order = [];
+    var set = new util.Set();
+    set.add(4.2);
+    set.add(6.4);
+    set.add(6.1);
+    async.groupBySeries(set, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [4.2],
+        '6': [6.4, 6.1]
+      });
+      assert.deepEqual(order, [4.2, 6.4, 6.1]);
+      done();
+    });
+  });
+
+  it('should execute iterator to series by collection of Set with passing key', function(done) {
+    var order = [];
+    var set = new util.Set();
+    set.add(4.2);
+    set.add(6.4);
+    set.add(6.1);
+    async.groupBySeries(set, groupByIteratorWithKey(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [4.2],
+        '6': [6.4, 6.1]
+      });
+      assert.deepEqual(order, [
+        [4.2, 0],
+        [6.4, 1],
+        [6.1, 2]
+      ]);
+      done();
+    });
+  });
+
+  it('should execute iterator to series by collection of Map', function(done) {
+
+    var order = [];
+    var map = new util.Map();
+    map.set('a', 4.2);
+    map.set('b', 6.4);
+    map.set('c', 6.1);
+    async.groupBySeries(map, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [['a',4.2]],
+        '6': [['b', 6.4], ['c', 6.1]]
+      });
+      assert.deepEqual(order, [
+        ['a', 4.2],
+        ['b', 6.4],
+        ['c', 6.1]
+      ]);
+      done();
+    });
+  });
+
+  it('should execute iterator to series by collection of Map with passing key', function(done) {
+    var order = [];
+    var map = new util.Map();
+    map.set('a', 4.2);
+    map.set('b', 6.4);
+    map.set('c', 6.1);
+    async.groupBySeries(map, groupByIteratorWithKey(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [['a',4.2]],
+        '6': [['b', 6.4], ['c', 6.1]]
+      });
+      assert.deepEqual(order, [
+        [['a', 4.2], 0],
+        [['b', 6.4], 1],
+        [['c', 6.1], 2]
+      ]);
+      done();
+    });
+  });
+
+  it('should throw error', function(done) {
+
+    var order = [];
+    var collection = [4.4, 6.4, 4, 5];
+    var iterator = function(num, callback) {
+      setTimeout(function() {
+        order.push(num);
+        callback(num === 5, Math.floor(num));
+      }, num * delay);
+    };
+
+    async.groupBySeries(collection, iterator, function(err, res) {
+      assert.ok(err);
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {
+        '4': [4.4, 4],
+        '6': [6.4]
+      });
+      assert.deepEqual(order, [4.4, 6.4, 4, 5]);
+      done();
+    });
+  });
+
+  it('should throw error if double callback', function(done) {
+
+    var errorCallCount = 0;
+    setTimeout(function() {
+      assert.strictEqual(errorCallCount, 2);
+      done();
+    }, delay);
+
+    domain.create()
+      .on('error', util.errorChecker)
+      .on('error', function() {
+        errorCallCount++;
+      })
+      .run(function() {
+        var collection = [1, 3];
+        var iterator = function(num, callback) {
+          process.nextTick(function() {
+            callback(null, num);
+          });
+          process.nextTick(function() {
+            callback(null, num);
+          });
+        };
+        async.groupBySeries(collection, iterator);
+      });
+  });
+
+  it('should avoid double callback', function(done) {
+
+    var called = false;
+    async.groupBySeries([1, 2, 3], function(item, callback) {
+      try {
+        callback(item);
+      } catch (exception) {
+        try {
+          callback(exception);
+        } catch(e) {
+          assert.ok(e);
+          util.errorChecker(e);
+          done();
+        }
+      }
+    }, function(err) {
+      assert.ok(err);
+      assert.strictEqual(called, false);
+      called = true;
+      async.nothing();
+    });
+  });
+
+  it('should return response immediately if array is empty', function(done) {
+
+    var order = [];
+    var array = [];
+    async.groupBySeries(array, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {});
+      assert.deepEqual(order, []);
+      done();
+    });
+  });
+
+  it('should return response immediately if object is empty', function(done) {
+
+    var order = [];
+    var object = {};
+    async.groupBySeries(object, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {});
+      assert.deepEqual(order, []);
+      done();
+    });
+  });
+
+  it('should return response immediately if collection is function', function(done) {
+
+    var order = [];
+    async.groupBySeries(function() {}, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {});
+      assert.deepEqual(order, []);
+      done();
+    });
+  });
+
+  it('should return response immediately if collection is undefined', function(done) {
+
+    var order = [];
+    async.groupBySeries(undefined, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {});
+      assert.deepEqual(order, []);
+      done();
+    });
+  });
+
+  it('should return response immediately if collection is null', function(done) {
+
+    var order = [];
+    async.groupBySeries(null, groupByIterator(order), function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+      assert.deepEqual(res, {});
+      assert.deepEqual(order, []);
+      done();
+    });
+  });
+
+});
