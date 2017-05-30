e40d32b8fb1dbe7993834867a11d5c603b720c92
Suguru Motegi <suguru.motegi@gmail.com>
2017-05-29 23:15:24 -0700

    chore(release): fix the release task

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -10,17 +10,30 @@ const bump = require('gulp-bump');
 const runSequence = require('run-sequence');
 const tagVersion = require('gulp-tag-version');
 
-const packagepath = './package.json';
+const basepath = path.resolve(__dirname, '../../');
+const packagepath = path.resolve(basepath, 'package.json');
+const bowerpath = path.resolve(basepath, 'bower.json');
 const types = ['patch', 'minor', 'major'];
+const { version: prevVersion } = require(packagepath);
 
 _.forEach(types, type => {
-  gulp.task(`release:package:${type}`, updateVersion(type));
+  gulp.task(`release:package:${type}`, updatePackageVersion(type));
   gulp.task(`release:${type}`, () => runSequence(
     `release:package:${type}`,
+    'release:async',
     'minify:local',
+    'release:dist',
     'release:commit',
     'release:tag'
   ));
+});
+
+gulp.task('release:async', () => {
+  delete require.cache[packagepath];
+  const { version } = require(packagepath);
+  const asyncpath = path.resolve(basepath, 'lib', 'async.js');
+  const file = fs.readFileSync(asyncpath, 'utf8');
+  fs.writeFileSync(asyncpath, file.replace(new RegExp(prevVersion, 'g'), version), 'utf8');
 });
 
 gulp.task('release:tag', () => {
@@ -30,24 +43,23 @@ gulp.task('release:tag', () => {
 
 gulp.task('release:dist', () => {
   _.forEach(['async.js', 'async.min.js'], file => {
-    const filepath = path.resolve(__dirname, '../..', 'lib', file);
-    const distpath = path.resolve(__dirname, '../..', 'dist', file);
+    const filepath = path.resolve(basepath, 'lib', file);
+    const distpath = path.resolve(basepath, 'dist', file);
     fs.createReadStream(filepath)
       .pipe(fs.createWriteStream(distpath));
   });
 });
 
 gulp.task('release:commit', () => {
-  const packagepath = path.resolve(__dirname, '../..', 'package.json');
   delete require.cache[packagepath];
   const { version } = require(packagepath);
-  return gulp.src(['./dist/*', packagepath])
+  return gulp.src(['./dist/*', './lib/*', packagepath, bowerpath])
     .pipe(git.commit(version));
 });
 
-function updateVersion(type) {
+function updatePackageVersion(type) {
   return () => {
-    return gulp.src(packagepath)
+    return gulp.src([packagepath, bowerpath])
         .pipe(bump({ type }))
         .pipe(gulp.dest('./'));
   };
